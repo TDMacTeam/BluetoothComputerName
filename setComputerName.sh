2c67a3beedcd26eef1e887bd3fa830d9fd4a3f45
TDMacTeam <n.olander@techdefenders.com>
2017-07-10 11:00:20 -0400

    Updated setup script to move script to /opt & set permissions/ownership Updated plist to match /opt location

--- setComputerName.sh
+++ setComputerName.sh
@@ -1,1 +1,3 @@
+#!/bin/sh
+
 scutil --set ComputerName "Test's Mac $(date +%F_%T)"
--- setup.sh
+++ setup.sh
@@ -2,8 +2,15 @@
 echo "Copying plist..."
 sudo cp ./setComputerName.plist /Library/LaunchDaemons/
 
-echo "Copying bash script..."
-cp ./setComputerName.sh ~/
+echo "Copying shell script..."
+cp ./setComputerName.sh /opt/
+
+echo "Updating permissions on script..."
+sudo chown root:root /opt/setComputerName.sh
+sudo chmod +rwx /opt/setComputerName.sh
 
 echo "Installing plist with launchctl..."
 sudo launchctl -w load /Library/LaunchDaemons/setComputerName.plist
+
+clear
+echo "Complete!"
